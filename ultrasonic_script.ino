15c3acba8a939a2326e83fb8d66c202f334735e9
kylemcshea <kmcshea@stevens.edu>
2020-10-09 16:53:59 -0400

    Made it so that it renders well with the python script

--- ultrasonic_script.ino
+++ ultrasonic_script.ino
@@ -1,4 +1,9 @@
-//Ultrasonic Sensor #1
+//import processing.serial;
+//Table dataTable; //Table we use for csv
+//int numReadings = 500; //number of readings taken before publishing data.
+
+//String fileName;
+
 const int trigPin[] = {2,4,6,8,10,12}; // Trigger Pin of Ultrasonic Sensor(s)
 const int echoPin[] = {3,5,7,9,11,13}; // Echo Pin of Ultrasonic Sensor(s)
 
@@ -13,12 +18,15 @@ float distance[numberOfSensors];
 float distancePrev[numberOfSensors];
 
 void setup() {
+  //table.addColumn("Count");
   for (int i =0;i<numberOfSensors;i++){
     pinMode(trigPin[i],OUTPUT);
     pinMode(echoPin[i],INPUT);
+    //table.addColumn("Sensor ["+String(i)+"]");
   }
   Serial.begin(9600); // Starting Serial Terminal
 }
+//Grabs distance from sensor reading
 int getDistance(int trigPin, int echoPin){
   float duration, distanceTemp;
   digitalWrite(trigPin, LOW);
@@ -31,9 +39,10 @@ int getDistance(int trigPin, int echoPin){
   return distanceTemp;
 }
 void loop() {
-
-  //Ultrasonic Sensor 1 functions
-  for(int i = 0; i < numberOfSensors; i++){
+  //Ultrasonic Sensor functions
+  //TableRow newRow = dataTable.addRow(); //Create row for input 
+  //newRow.setInt("Count",counter); //Indicates what count readings are for in csv
+  for(int i = 0; i < numberOfSensors; i++){ //Grabs distance from each sensor
     distancePrev[i] =distance[i];
     distance[i] = getDistance(trigPin[i], echoPin[i]);
     if(counter > 100){
@@ -42,6 +51,7 @@ void loop() {
         Serial.print("Reseting to previous distance at sensor " + String(i));
       }
     }
+    //newRow.setInt("Sensor ["+String(i)+"]",distance[i]);
     if(counter < 100){
       base[i]+= distance[i];
     }
@@ -51,12 +61,15 @@ void loop() {
       delay(2000);
     }
   }
-  Serial.print("["+String(counter)+"] ");
+  Serial.print(String(counter)+"| ");
   for(int j = 0; j < numberOfSensors; j++){
     Serial.print("Sensor " + String(j) + ": " + String(distance[j]) + " | ");
   }
   Serial.println();
-   
+  //if(Counter%numReadings == 0){
+    //fileName = String(year())+ String(month())+String(day()) + String(Counter/numReadings);
+    //saveTable(dataTable,fileName);
+  //}
   counter++;
   delay(200);
 }
