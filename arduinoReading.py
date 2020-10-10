a875a8f46085b7023f1323e935d28d17dd9640bc
Jack <60860781+jack-nonnie@users.noreply.github.com>
2020-10-10 11:42:38 -0400

    Seaborn

--- arduinoReading.py
+++ arduinoReading.py
@@ -2,6 +2,9 @@ import serial
 import time
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
 ser = serial.Serial('COM5',9600)
 time.sleep(2)
 b=ser.readline()
@@ -17,10 +20,14 @@ for i in range(5):
         lineOfTable.append(float(s[a][11:]))
     lintOfTable = np.array(lineOfTable)
     data.append(lineOfTable)
+
+#data = [np.array([0, 242.0, 234.0, 242.0,240.0,239.0]), np.array([1, 240.0, 233.0, 240.0,242.0,241.0]), np.array([2, 232.0, 134.0, 240.0,237.0,235.0]), np.array([3, 245.0, 240.0, 242.0,241.0,220.0]), np.array([4, 244.0, 244.0, 243.0,241.0,240.0]), np.array([5, 239.0, 140.0, 240.0,241.0,229.0])]
 column = ["Count"]
 for col in range(1, len(data[0])):
     column.append("Sensor " + str(col - 1))
 
 data = np.array(data)
 df = pd.DataFrame(data, columns = column)
-print(df)
+dfnotefic = df.drop("Count", axis=1)
+sns.scatterplot(data= dfnotefic)
+plt.show()
